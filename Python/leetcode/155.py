eb8e72471f851ec1b49485f8ba17cc9931497b23
Saransh Chopra <saransh0701@gmail.com>
2026-06-19 21:15:33 +0200

    another minstack impl

--- Python/leetcode/155.py
+++ Python/leetcode/155.py
@@ -22,6 +22,36 @@ class MinStack:
         return self.data[-1][1]
 
 
+class MinStack:
+
+    def __init__(self):
+        self.data = deque()
+        self.min = 0
+
+    def push(self, val: int) -> None:
+        if len(self.data) == 0:
+            self.data.append(val)
+            self.min = val
+        elif val < self.min:
+            self.data.append(2*val - self.min)
+            self.min = val
+        else:
+            self.data.append(val)
+
+    def pop(self) -> None:
+        if self.data[-1] < self.min:
+            self.min = 2 * self.min - self.data[-1]
+        self.data.pop()
+
+    def top(self) -> int:
+        if self.data[-1] <= self.min:
+            return self.min
+        return self.data[-1]
+
+    def getMin(self) -> int:
+        return self.min
+
+
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(val)
